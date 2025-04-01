56eaabdc19483b90d6aa8e67dd6a30fd4d281400
ZeynepSudeHaksal <haksalzeynepsude@gmail.com>
2025-04-01 03:38:29 -0400

    yeto

--- a3.c
+++ a3.c
@@ -4,61 +4,64 @@
 #include <string.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
 #include "memory.h"
 #include "cpu.h"
+#include "cores.h"
 #include "graph.h"
 
 #define READ_END 0
 #define WRITE_END 1
 
 int main(int argc, char *argv[]) {
-    int samples = 20;  // Default number of samples
-    int tdelay = 500000;  // Default delay in microseconds (0.5 seconds)
-    int mem_flag = 0;  // Monitor memory?
-    int cpu_flag = 0;  // Monitor CPU?
-    int cores_flag = 0;  // Monitor cores? Not implemented in detailed monitoring here
+    int samples = 20;             // Default number of samples
+    int tdelay = 500000;          // Default delay in microseconds (0.5 seconds)
+    int mem_flag = 0;             // Monitor memory?
+    int cpu_flag = 0;             // Monitor CPU?
+    int cores_flag = 0;           // Monitor cores?
 
-    // Parse command-line arguments
+    // Command-line parsing
     for (int i = 1; i < argc; i++) {
-        if ((i==1) && (isdigit(*argv[i]))) //parses the CLA arguments.
-        {samples = atoi(argv[i]);}
-
-        else if ((i==2) && (isdigit(*argv[i])))
-        {tdelay = atoi(argv[i]);
-        } else if (strcmp(argv[i], "--memory") == 0) {
-            mem_flag = 1;
-        } else if (strcmp(argv[i], "--cpu") == 0) {
-            cpu_flag = 1;
-        } else if (strcmp(argv[i], "--cores") == 0) {
-            cores_flag = 1;
-        }
+        if (i == 1 && isdigit(*argv[i])) samples = atoi(argv[i]);
+        else if (i == 2 && isdigit(*argv[i])) tdelay = atoi(argv[i]) * 1000;
+        else if (strcmp(argv[i], "--memory") == 0) mem_flag = 1;
+        else if (strcmp(argv[i], "--cpu") == 0) cpu_flag = 1;
+        else if (strcmp(argv[i], "--cores") == 0) cores_flag = 1;
     }
 
     if (!mem_flag && !cpu_flag && !cores_flag) {
-        mem_flag = cpu_flag = cores_flag = 1; // Default to monitor all if no specific flags are given
+        mem_flag = cpu_flag = cores_flag = 1;  // default to all
     }
 
-    int mem_pipe[2], cpu_pipe[2];
-    pid_t mem_pid, cpu_pid;
+    // Pipes and PIDs
+    int mem_pipe[2], cpu_pipe[2], core_pipe[2];
+    pid_t mem_pid, cpu_pid, core_pid;
 
-    // Create pipes
+    // MEMORY pipe
     if (mem_flag && pipe(mem_pipe) == -1) {
-        perror("Failed to create memory pipe");
-        exit(EXIT_FAILURE);
-    }
-    if (cpu_flag && pipe(cpu_pipe) == -1) {
-        perror("Failed to create CPU pipe");
+        perror("Memory pipe failed");
         exit(EXIT_FAILURE);
     }
 
-    // Fork memory monitoring process
+    // CPU pipe
+    if (cpu_flag && pipe(cpu_pipe) == -1) {
+        perror("CPU pipe failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // CORES pipe
+    if (cores_flag && pipe(core_pipe) == -1) {
+        perror("Cores pipe failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Memory child
     if (mem_flag) {
         mem_pid = fork();
-        if (mem_pid == 0) {  // Child process for memory monitoring
+        if (mem_pid == 0) {
             close(mem_pipe[READ_END]);
             long int used_memory, total_memory;
-
             while (1) {
                 get_memory_usage(&used_memory, &total_memory);
                 write(mem_pipe[WRITE_END], &used_memory, sizeof(long int));
@@ -71,13 +74,12 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Fork CPU monitoring process
+    // CPU child
     if (cpu_flag) {
         cpu_pid = fork();
-        if (cpu_pid == 0) {  // Child process for CPU monitoring
+        if (cpu_pid == 0) {
             close(cpu_pipe[READ_END]);
             double cpu_utilization;
-
             while (1) {
                 cpu_utilization = calculate_cpu_utilization();
                 write(cpu_pipe[WRITE_END], &cpu_utilization, sizeof(double));
@@ -89,12 +91,36 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // Cores child
+    int num_cores = 0, max_freq = 0;
+    if (cores_flag) {
+        core_pid = fork();
+        if (core_pid == 0) {
+            close(core_pipe[READ_END]);
+            get_core_info(&num_cores, &max_freq);
+            write(core_pipe[WRITE_END], &num_cores, sizeof(int));
+            write(core_pipe[WRITE_END], &max_freq, sizeof(int));
+            close(core_pipe[WRITE_END]);
+            exit(0);  // only needs to write once
+        } else {
+            close(core_pipe[WRITE_END]);
+        }
+    }
+
+    // Arrays and variables for storing data
     long int memory_usage, overall_memory = 0;
     double cpu_usage;
     long int memo_util_arr[samples];
     double cpu_value_arr[samples];
 
-    // Main loop to read data from pipes and graph dynamically
+    // Read core info once
+    if (cores_flag) {
+        read(core_pipe[READ_END], &num_cores, sizeof(int));
+        read(core_pipe[READ_END], &max_freq, sizeof(int));
+        close(core_pipe[READ_END]);
+    }
+
+    // Sampling loop
     for (int i = 0; i < samples; i++) {
         if (mem_flag) {
             if (read(mem_pipe[READ_END], &memory_usage, sizeof(long int)) > 0 &&
@@ -108,8 +134,11 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        graph(samples, tdelay, mem_flag, cpu_flag, cores_flag, 0, memo_util_arr, overall_memory, 0, cpu_value_arr, i);
-        usleep(tdelay); // Update graph at intervals
+        graph(samples, tdelay, mem_flag, cpu_flag, cores_flag,
+              num_cores, memo_util_arr, overall_memory, max_freq,
+              cpu_value_arr, i);
+
+        usleep(tdelay);
     }
 
     // Cleanup
@@ -121,8 +150,10 @@ int main(int argc, char *argv[]) {
         close(cpu_pipe[READ_END]);
         kill(cpu_pid, SIGKILL);
     }
+    if (cores_flag) {
+        kill(core_pid, SIGKILL);  // technically not needed as it exits immediately
+    }
 
-    while (wait(NULL) > 0); // Wait for all child processes to exit
-
+    while (wait(NULL) > 0);  // Wait for all children to finish
     return 0;
 }
