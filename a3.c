4934245dc55040abd62a49fd166686f33a44cae9
ZeynepSudeHaksal <haksalzeynepsude@gmail.com>
2025-04-01 00:04:09 -0400

    blaaahahh

--- a3.c
+++ a3.c
@@ -25,8 +25,6 @@ int main(int argc, char *argv[]) {
     int samp = 20; // Number of samples
     int delay = 500000; // Delay in microseconds between samples
     int mem = 0, cp = 0, core = 0;
-    long int total_memory, used_memory, total, idle;
-    int num_cores, max_freq;
 
     // Parse command-line arguments
     for (int i = 1; i < argc; i++) {
@@ -48,14 +46,14 @@ int main(int argc, char *argv[]) {
             delay = atoi(argv[i] + 9);
         } else {
             fprintf(stderr, "Invalid argument: %s\n", argv[i]);
-            return 1;
+            exit(1);
         }
     }
 
     signal(SIGINT, handle_sigint);
     signal(SIGTSTP, handle_sigtstp);
 
-    // Setup data arrays for graphing
+    // Memory and CPU usage arrays
     long int* memory_usage_array = malloc(samp * sizeof(long int));
     double* cpu_usage_array = malloc(samp * sizeof(double));
     if (memory_usage_array == NULL || cpu_usage_array == NULL) {
@@ -63,7 +61,7 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    // Setup pipes
+    // Set up pipes
     int pipes[3][2];
     for (int i = 0; i < 3; i++) {
         if (pipe(pipes[i]) == -1) {
@@ -80,16 +78,19 @@ int main(int argc, char *argv[]) {
             return 1;
         } else if (pid == 0) { // Child process
             close(pipes[i][0]); // Close read end in child
-
-            if (i == 0 && mem) { // Memory process
+            // Collect and send data
+            if (i == 0 && mem) {
+                long int used_memory, total_memory;
                 get_memory_usage(&used_memory, &total_memory);
                 write(pipes[i][1], &used_memory, sizeof(used_memory));
                 write(pipes[i][1], &total_memory, sizeof(total_memory));
-            } else if (i == 1 && cp) { // CPU process
+            } else if (i == 1 && cp) {
+                long int total, idle;
                 read_cpu_times(&total, &idle);
                 write(pipes[i][1], &total, sizeof(total));
                 write(pipes[i][1], &idle, sizeof(idle));
-            } else if (i == 2 && core) { // Cores process
+            } else if (i == 2 && core) {
+                int num_cores, max_freq;
                 get_core_info(&num_cores, &max_freq);
                 write(pipes[i][1], &num_cores, sizeof(num_cores));
                 write(pipes[i][1], &max_freq, sizeof(max_freq));
@@ -101,17 +102,18 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Read from pipes in parent
+    // Read data from child processes
+    int num_cores, max_freq;
+    long int total_memory = 0;
     for (int i = 0; i < 3; i++) {
         if (i == 0 && mem) {
-            read(pipes[i][0], &used_memory, sizeof(used_memory));
-            read(pipes[i][0], &total_memory, sizeof(total_memory));
+            read(pipes[i][0], &memory_usage_array[0], sizeof(long int));
+            read(pipes[i][0], &total_memory, sizeof(long int));
         } else if (i == 1 && cp) {
-            read(pipes[i][0], &total, sizeof(total));
-            read(pipes[i][0], &idle, sizeof(idle));
+            read(pipes[i][0], &cpu_usage_array[0], sizeof(double));
         } else if (i == 2 && core) {
-            read(pipes[i][0], &num_cores, sizeof(num_cores));
-            read(pipes[i][0], &max_freq, sizeof(max_freq));
+            read(pipes[i][0], &num_cores, sizeof(int));
+            read(pipes[i][0], &max_freq, sizeof(int));
         }
         close(pipes[i][0]);
     }
@@ -121,24 +123,18 @@ int main(int argc, char *argv[]) {
         wait(NULL);
     }
 
-    // Graphing data
+    // Graph the results
     if (mem || cp) {
-        for (int j = 0; j < samp; j++) {
-            memory_usage_array[j] = used_memory; // Example to fill data
-            cpu_usage_array[j] = (double)(total - idle) / total * 100.0;
-            graph(samp, delay, mem, cp, core, num_cores, memory_usage_array, total_memory, max_freq, cpu_usage_array, j);
-        }
-        
+        graph(samp, delay, mem, cp, core, num_cores, memory_usage_array, total_memory, max_freq, cpu_usage_array, samp - 1);
     }
     if (core) {
         draw_cores(num_cores);
     }
 
-    // Free allocated memory
+    // Clean up
     free(memory_usage_array);
     free(cpu_usage_array);
 
-    // Handle termination signal
     if (quit) {
         printf("Termination signal received, exiting program...\n");
         return 0;
