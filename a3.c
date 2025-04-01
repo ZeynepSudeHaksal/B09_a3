a5fe832453dc992413dee4b023980d08ba9fb479
ZeynepSudeHaksal <haksalzeynepsude@gmail.com>
2025-04-01 03:25:13 -0400

    pufff

--- a3.c
+++ a3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <signal.h>
 #include <sys/wait.h>
 
@@ -34,27 +34,34 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Set default to monitor all if no specific flags are given
     if (!mem_flag && !cpu_flag && !cores_flag) {
-        mem_flag = cpu_flag = cores_flag = 1;
+        mem_flag = cpu_flag = cores_flag = 1; // Default to monitor all if no specific flags are given
     }
 
     int mem_pipe[2], cpu_pipe[2];
     pid_t mem_pid, cpu_pid;
 
-    // Create pipes and fork processes if flags are set
+    // Create pipes
+    if (mem_flag && pipe(mem_pipe) == -1) {
+        perror("Failed to create memory pipe");
+        exit(EXIT_FAILURE);
+    }
+    if (cpu_flag && pipe(cpu_pipe) == -1) {
+        perror("Failed to create CPU pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    // Fork memory monitoring process
     if (mem_flag) {
-        if (pipe(mem_pipe) == -1) {
-            perror("Failed to create memory pipe");
-            exit(EXIT_FAILURE);
-        }
         mem_pid = fork();
-        if (mem_pid == 0) {  // Memory monitoring child process
+        if (mem_pid == 0) {  // Child process for memory monitoring
             close(mem_pipe[READ_END]);
             long int used_memory, total_memory;
+
             while (1) {
                 get_memory_usage(&used_memory, &total_memory);
                 write(mem_pipe[WRITE_END], &used_memory, sizeof(long int));
+                write(mem_pipe[WRITE_END], &total_memory, sizeof(long int));
                 usleep(tdelay);
             }
             exit(0);
@@ -63,15 +70,13 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // Fork CPU monitoring process
     if (cpu_flag) {
-        if (pipe(cpu_pipe) == -1) {
-            perror("Failed to create CPU pipe");
-            exit(EXIT_FAILURE);
-        }
         cpu_pid = fork();
-        if (cpu_pid == 0) {  // CPU monitoring child process
+        if (cpu_pid == 0) {  // Child process for CPU monitoring
             close(cpu_pipe[READ_END]);
             double cpu_utilization;
+
             while (1) {
                 cpu_utilization = calculate_cpu_utilization();
                 write(cpu_pipe[WRITE_END], &cpu_utilization, sizeof(double));
@@ -83,18 +88,17 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    long int memory_usage;
+    long int memory_usage, overall_memory = 0;
     double cpu_usage;
-    long int overall_memory = 0;  // Updated dynamically if needed
     long int memo_util_arr[samples];
     double cpu_value_arr[samples];
 
     // Main loop to read data from pipes and graph dynamically
     for (int i = 0; i < samples; i++) {
         if (mem_flag) {
-            if (read(mem_pipe[READ_END], &memory_usage, sizeof(long int)) > 0) {
+            if (read(mem_pipe[READ_END], &memory_usage, sizeof(long int)) > 0 &&
+                read(mem_pipe[READ_END], &overall_memory, sizeof(long int)) > 0) {
                 memo_util_arr[i] = memory_usage;
-                if (i == 0) overall_memory = memory_usage;
             }
         }
         if (cpu_flag) {
@@ -103,7 +107,6 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        // Graph the results dynamically
         graph(i + 1, tdelay, mem_flag, cpu_flag, cores_flag, 0, memo_util_arr, overall_memory, 0, cpu_value_arr, i);
         usleep(tdelay); // Update graph at intervals
     }
